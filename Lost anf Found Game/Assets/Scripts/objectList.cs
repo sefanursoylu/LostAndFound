bb583945e03d51159876b7b80596aa13b65e3d0a
Sefa <sefanursoylu76@gmail.com>
2021-08-24 20:00:17 +1000

    Character Instantiate
    got basic instantiate for characters working

---
+++ Lost anf Found Game/Assets/Scripts/objectList.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectList : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+}
