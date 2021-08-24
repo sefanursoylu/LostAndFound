bdb0afd946daf2d872219005315e5ef07f6e2b98
Sefa <sefanursoylu76@gmail.com>
2021-08-24 21:00:03 +1000

    object instantiate

--- Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
+++ Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class characterInstantiate : MonoBehaviour
 {
     public characterList charList;
-    public GameObject objectList;
+    public customerList objectList;
     public GameObject ob;
+    public Transform goOB;
+    private GameObject obj_;
 
     void Start()
     {
         for (var i = 0; i < 1; i++)
         {
             ob = Instantiate(charList.characters[Random.Range(0, charList.characters.Count)], transform.position, Quaternion.identity);
-            //ob.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+            goOB = ob.gameObject.transform.GetChild(0).transform;
+            obj_ = Instantiate(objectList.customers[Random.Range(0, objectList.customers.Count)], goOB.position, Quaternion.identity);
+            obj_.transform.SetParent(ob.transform);
         }
+    }
+
+    void Update()
+    {
+        
     }
 }
