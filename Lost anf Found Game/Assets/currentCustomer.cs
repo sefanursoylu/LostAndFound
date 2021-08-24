a2b2fc0c630b2fcadbc11358627db90b8b36265f
Merve <mervenur.soylu@gmail.com>
2021-08-24 20:00:10 +1000

    Added Placeholder Objects and Customer Prefabs and some code

---
+++ Lost anf Found Game/Assets/currentCustomer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class currentCustomer : MonoBehaviour
+{
+    public GameObject customer;
+
+    void Update()
+    {
+        
+    }
+}
