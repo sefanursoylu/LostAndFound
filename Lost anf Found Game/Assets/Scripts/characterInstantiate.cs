24586e0b7bf40c115537d56a3d642bc73e6ff54b
Sefa <sefanursoylu76@gmail.com>
2021-08-26 21:45:29 +1000

    character instantiate script update

--- Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
+++ Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
@@ -10,6 +10,8 @@ public class characterInstantiate : MonoBehaviour
     public Transform goOB;
     private GameObject obj_;
     private Color oldColor;
+    public currentCustomer currOb;
+    public GameObject curO;
 
     void Start()
     {
@@ -19,20 +21,39 @@ public class characterInstantiate : MonoBehaviour
             goOB = ob.gameObject.transform.GetChild(0).transform;
             obj_ = Instantiate(objectList.customers[Random.Range(0, objectList.customers.Count)], goOB.position, Quaternion.identity);
             obj_.transform.SetParent(ob.transform);
+
+            
         }
+
+ 
+        
+
     }
 
     void Update()
     {
+        
         Destroy(ob, 10);
         
         if (ob == false)
         {
-            ob = Instantiate(charList.characters[Random.Range(0, charList.characters.Count)], transform.position, Quaternion.identity);
-            goOB = ob.gameObject.transform.GetChild(0).transform;
-            obj_ = Instantiate(objectList.customers[Random.Range(0, objectList.customers.Count)], goOB.position, Quaternion.identity);
-            obj_.transform.SetParent(ob.transform);
+            for (var i = 0; i < 1; i++)
+            {
+                ob = Instantiate(charList.characters[Random.Range(0, charList.characters.Count)], transform.position, Quaternion.identity);
+                goOB = ob.gameObject.transform.GetChild(0).transform;
+                obj_ = Instantiate(objectList.customers[Random.Range(0, objectList.customers.Count)], goOB.position, Quaternion.identity);
+                obj_.transform.SetParent(ob.transform);
+
+                if (obj_.tag == objectList.customers[i].tag)
+                {
+                    objectList.customers.Remove(objectList.customers[i]);
+                }
+
+            }
+
         }
+
+        
 
         oldColor = ob.GetComponent<Renderer>().material.color;
         ob.GetComponent<Renderer>().material.color = Color.Lerp(oldColor, Color.red, Time.deltaTime / 10f);
