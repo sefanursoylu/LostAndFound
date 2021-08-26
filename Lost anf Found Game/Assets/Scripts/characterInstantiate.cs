2adf3fe8285e261def01a4680c0b75ec22656abe
Sefa <sefanursoylu76@gmail.com>
2021-08-26 15:46:11 +1000

    instantiate loop with customers, and correct item found basic logic

--- Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
+++ Lost anf Found Game/Assets/Scripts/characterInstantiate.cs
@@ -9,6 +9,7 @@ public class characterInstantiate : MonoBehaviour
     public GameObject ob;
     public Transform goOB;
     private GameObject obj_;
+    private Color oldColor;
 
     void Start()
     {
@@ -23,6 +24,17 @@ public class characterInstantiate : MonoBehaviour
 
     void Update()
     {
+        Destroy(ob, 10);
         
+        if (ob == false)
+        {
+            ob = Instantiate(charList.characters[Random.Range(0, charList.characters.Count)], transform.position, Quaternion.identity);
+            goOB = ob.gameObject.transform.GetChild(0).transform;
+            obj_ = Instantiate(objectList.customers[Random.Range(0, objectList.customers.Count)], goOB.position, Quaternion.identity);
+            obj_.transform.SetParent(ob.transform);
+        }
+
+        oldColor = ob.GetComponent<Renderer>().material.color;
+        ob.GetComponent<Renderer>().material.color = Color.Lerp(oldColor, Color.red, Time.deltaTime / 10f);
     }
 }
--- Lost anf Found Game/Assets/Scripts/objectBehaviour.cs
+++ Lost anf Found Game/Assets/Scripts/objectBehaviour.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class objectBehaviour : MonoBehaviour
 {
-    private GameObject currentCustomer;
+    public GameObject currentCustomer;
     public currentCustomer customerChecker;
     public PlayerHealth HP;
+
+    public void Update()
+    {
+        currentCustomer = GameObject.FindGameObjectWithTag("Customer");
+    }
 
     public void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0) && (gameObject.tag == customerChecker.currentTag))
         {
             Debug.Log("Correct Object!");
+            Destroy(currentCustomer);
             Destroy(gameObject);
         } 
         
